c5ebad5db3f54e881e22839a32566b74310b4e73
AnaClaudiaCabral <traderclaudynha@gmail.com>
2025-08-22 14:11:51 +0000

    Desafio nivel aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,27 +9,27 @@ int main() {
     // Cadastro das Cartas: duas cartas distintas
     // Correção das variáves!
             
-char estado1[20], codigo1[20], cidade1[20];
+char estado1, codigo1[20], cidade1[50];
 int populacao1, turismo1;
-float area1, pib1;  // dados carta 1
+float area1, pib1, densidadepopulacional1, pibpercapita1;  // dados carta 1
 
-char estado2[20], codigo2[20], cidade2[20];
+char estado2, codigo2[20], cidade2[50];
 int populacao2, turismo2;
-float area2, pib2;   // dados carta 2
+float area2, pib2, densidadepopulacional2, pibpercapita2;   // dados carta 2
 
 
-     printf("Desafio Super Trunfo - Nível 1\n");
+     printf("<<<< Desafio Super Trunfo - Nível 2 >>>>\n");
 
-        printf("Carta 1\n");
+        printf("*** Carta 1 ***\n");
 
         printf("De A a H, informe uma letra representando um dos oito Estados Brasileiro: \n");
-        scanf(" %s", estado1);
+        scanf(" %c", &estado1);
 
         printf("Informe a letra escolhida do Estado seguida de um número de 01 a 04: \n");
-        scanf(" %s", codigo1);
+        scanf(" %s", &codigo1);
 
         printf("Informe o nome de uma cidade do Estado informado anteriormente: \n");
-        scanf(" %s", cidade1);
+        scanf(" %s", &cidade1);
 
         printf("Informe o número de habitantes desta Cidade: \n");
         scanf(" %d", &populacao1);
@@ -42,28 +42,35 @@ float area2, pib2;   // dados carta 2
 
         printf("Informe a quantidade de pontos turísticos existentes nesta cidade: \n");
         scanf(" %i", &turismo1);
+
+        densidadepopulacional1 = (populacao1)/area1;
+        pibpercapita1 = (pib1)/populacao1;
             
           //Bloco de saída de dados carta 1
         printf("<<< Dados da Carta 1 >>>\n");
+        printf("Estado: %c\n", estado1);
         printf("Código Estado: %s\n", codigo1);
         printf("Nome da Cidade: %s\n", cidade1);
         printf("População: %d habitantes\n", populacao1);
         printf("Área: %.2f km²\n", area1);
-        printf("PIB: R$ %2.f\n", pib1);
+        printf("PIB: %.2f bilhões de reais\n", pib1);
         printf("Pontos Turísticos: %i\n",turismo1);
+        printf("Densidade Populacional: %.2f hab/km² \n", densidadepopulacional1);
+        printf("PIB per Capita: %.2f reais \n", pibpercapita1);
+
 
 
         // Dados carta 2
-        printf("Carta 2\n"); 
+        printf("*** Carta 2 ***\n"); 
         
         printf("De A a H, informe uma letra representando um dos oito Estados Brasileiro: \n");
-        scanf(" %s", estado2);
+        scanf(" %c", &estado2);
 
         printf("Informe a letra escolhida do Estado seguida de um número de 01 a 04: \n");
-        scanf(" %s", codigo2);
+        scanf(" %s", &codigo2);
 
         printf("Informe o nome de uma cidade do Estado informado anteriormente: \n");
-        scanf(" %s", cidade2);
+        scanf(" %s", &cidade2);
 
         printf("Informe o número de habitantes desta Cidade: \n");
         scanf(" %d", &populacao2);
@@ -77,14 +84,20 @@ float area2, pib2;   // dados carta 2
         printf("Informe a quantidade de pontos turísticos existentes nesta cidade: \n");
         scanf(" %i", &turismo2);
 
+        densidadepopulacional2 = (populacao2)/area2;
+        pibpercapita2 = (pib2)/populacao2;
+
         //Bloco de saída de dados carta 2
         printf("<<< Dados da Carta 2 >>>\n");
+        printf("Estado: %c\n", estado2);
         printf("Código Estado: %s\n", codigo2);
-        printf("Nome da Cidade: %s\n", cidade2);
+        printf("Nome da Cidade: %s", cidade2);
         printf("População: %d habitantes\n", populacao2);
         printf("Área: %.2f km²\n", area2);
-        printf("PIB: R$ %2.f\n", pib2);
+        printf("PIB: %.2f bilhões de reais\n", pib2);
         printf("Pontos Turisticos: %i\n",turismo2);
+        printf("Densidade Populacional: %.2f hab/km² \n", densidadepopulacional2);
+        printf("PIB per Capita: %.2f reais \n", pibpercapita2);
 
         printf("Novo Commit\n");
                       
